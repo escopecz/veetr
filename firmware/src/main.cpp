9f3998542a57e3cef75465463f24b597e31e38db
Jan Linhart <admin@escope.cz>
2025-11-07 22:51:37 +0100

    Bump to version 0.0.16

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -11,7 +11,7 @@
 #include <esp_ota_ops.h>
 
 // Firmware version
-#define FIRMWARE_VERSION "0.0.15"
+#define FIRMWARE_VERSION "0.0.16"
 
 // Debug flags - uncomment for verbose output
 // #define DEBUG_BLE_DATA
